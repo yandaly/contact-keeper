f7e6e84c6a8cda2f0af9f649c3acafeb0fa3324f
yandaly <thegreygringo@gmail.com>
2019-11-21 15:46:46 +0100

    adding mongoDB connection

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect DB
+connectDB();
+
 app.get('/', (req, res) =>
-   res.json({ msg: 'Welcome to the ContactKeeper API...' })
+	res.json({ msg: 'Welcome to the ContactKeeper API...' })
 );
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
-
 
 const PORT = process.env.PORT || 5000;
 
