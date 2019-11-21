b2d234739dd8c4e4ef501afca6728e9a8b66bb5e
yandaly <thegreygringo@gmail.com>
2019-11-21 17:30:43 +0100

    Creation of JWT

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
+const config = require('config');
 
 const User = require ('../models/User')
 
@@ -42,8 +44,16 @@ router.post('/', [
 
          await user.save();
 
-         res.send('User saved');
+         const payload = {
+            user: { id: user.id }
+         };
 
+         jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 360000,
+         }, (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+         });
       } catch (err) {
          console.log(err.message);
          res.status(500).send('Server error');
