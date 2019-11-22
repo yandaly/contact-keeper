93bf5de468d017149fec6e9bb8b789aacc194d63
yandaly <thegreygringo@gmail.com>
2019-11-22 16:57:07 +0100

    Delete route upgraded

--- routes/contacts.js
+++ routes/contacts.js
@@ -107,7 +107,7 @@ router.delete('/:id', auth, async (req, res) => {
 			return res.status(401).json({ msg: 'Access denied !' });
 		}
 
-		contact = await Contact.findByIdAndDelete(req.params.id);
+		await Contact.findByIdAndDelete(req.params.id);
 
 		res.json({ msg: 'Contact deleted' });
 	} catch (err) {
