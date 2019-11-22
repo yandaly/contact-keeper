0ea426889ce2913f25815f30951b1b9e868e9979
yandaly <thegreygringo@gmail.com>
2019-11-22 17:10:58 +0100

    Nothing

--- routes/contacts.js
+++ routes/contacts.js
@@ -109,7 +109,7 @@ router.delete('/:id', auth, async (req, res) => {
 
 		await Contact.findByIdAndRemove(req.params.id);
 
-		res.json({ msg: 'Contact deleted' });
+		res.json({ msg: 'Contact removed' });
 	} catch (err) {
 		console.error(err.message);
 		res.status(500).send('Server error');
