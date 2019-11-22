dcf775f8c30fa8169ce963c97d06d0a16a903ac4
yandaly <thegreygringo@gmail.com>
2019-11-22 10:19:15 +0100

    Auth middleware & route done

--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,19 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const config = require('config');
 const User = require('../models/User');
+const auth = require('../middleware/auth');
 
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Private
-router.get('/', (req, res) => {
-	res.send('Get logged in');
+router.get('/', auth, async (req, res) => {
+	try {
+		const user = await User.findById(req.user.id).select('-password');
+		res.json(user);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 });
 
 // @route   POST api/auth
