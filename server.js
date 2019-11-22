20cddb1deef30c4e7bc486fd301ba81791304baa
yandaly <thegreygringo@gmail.com>
2019-11-22 11:49:47 +0100

    Prettier on it

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 connectDB();
 
 // Init Middleware
-app.use(express.json({ extended: false }))
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) =>
 	res.json({ msg: 'Welcome to the ContactKeeper API...' })
