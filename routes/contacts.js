bba926cf7eace7b775a5d851ecab4f3ec33aae0e
yandaly <thegreygringo@gmail.com>
2019-11-22 14:28:05 +0100

    Added add contact

--- routes/contacts.js
+++ routes/contacts.js
@@ -28,11 +28,35 @@ router.get('/', auth, async (req, res) => {
 router.post(
 	'/',
 	[
-		check('name', 'Please include a name').exists()
+		auth,
+		[
+			check('name', 'Please include a name').not().isEmpty()
+		]
 	],
-	auth,
 	async (req, res) => {
-		res.send('HELLO');
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { name, email, phone, type } = req.body;
+
+		try {
+			const newContact = new Contact({
+				name,
+				email,
+				phone,
+				type,
+				user  : req.user.id
+			});
+
+			const contact = await newContact.save();
+
+			res.json(contact);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server error');
+		}
 	}
 );
 
