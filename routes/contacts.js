0e5c6545d69cb9b7ed76591b5876aeddef35ae4e
yandaly <thegreygringo@gmail.com>
2019-11-22 10:55:07 +0100

    Added imports

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,32 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
 
 // @route   GET api/contact
 // @desc    Get all user contacts
 // @access  Private
 router.get('/', (req, res) => {
-   res.send('Get all contacts');
+	res.send('Get all contacts');
 });
 
 // @route   POST api/contact
 // @desc    Add new contact
 // @access  Private
 router.post('/', (req, res) => {
-   res.send('Add contact');
+	res.send('Add contact');
 });
 
 // @route   PUT api/contact/:id
 // @desc    Update contact
 // @access  Private
 router.put('/:id', (req, res) => {
-   res.send('Update contact');
+	res.send('Update contact');
 });
 
 // @route   DELETE api/contact/:id
 // @desc    Delete contact
 // @access  Private
 router.delete('/:id', (req, res) => {
-   res.send('Delete contact');
+	res.send('Delete contact');
 });
 
 module.exports = router;
