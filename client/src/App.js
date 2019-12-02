3b70194b961be1c3df993afcf010f220088e74bd
yandaly <thegreygringo@gmail.com>
2019-12-02 15:02:54 +0100

    Contacts context and global state

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ContactState from './context/contact/ContactState';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
@@ -7,16 +8,18 @@ import './App.css';
 
 export default () => {
 	return (
-		<Router>
-			<Fragment>
-				<Navbar />
-				<div className='container'>
-					<Switch>
-						<Route exact path='/' component={Home} />
-						<Route exact path='/about' component={About} />
-					</Switch>
-				</div>
-			</Fragment>
-		</Router>
+		<ContactState>
+			<Router>
+				<Fragment>
+					<Navbar />
+					<div className='container'>
+						<Switch>
+							<Route exact path='/' component={Home} />
+							<Route exact path='/about' component={About} />
+						</Switch>
+					</div>
+				</Fragment>
+			</Router>
+		</ContactState>
 	);
 };
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -2,3 +2,63 @@ import React, { useReducer } from 'react';
 import uuid from 'uuid';
 import contactContext from './contactContext';
 import contactReducer from './contactReducer';
+import {
+	ADD_CONTACT,
+	DELETE_CONTACT,
+	CLEAR_CURRENT,
+	SET_CURRENT,
+	UPDATE_CONTACT,
+	FILTER_CONTACTS,
+	CLEAR_FILTER
+} from '../types';
+import { type } from 'os';
+
+const ContactState = (props) => {
+	const initialState = {
+		contacts : [
+			{
+				id    : 1,
+				name  : 'Emma Watson',
+				email : 'emma@watson.com',
+				phone : '0060060064',
+				type  : 'personal'
+			},
+			{
+				id    : 2,
+				name  : 'John Wick',
+				email : 'john@wick.com',
+				phone : '0070070076',
+				type  : 'professional'
+			}
+		]
+	};
+
+	const [
+		state,
+		dispatch
+	] = useReducer(contactReducer, initialState);
+
+	// Add contact
+
+	// Delete contact
+
+	// Set current contact
+
+	// Clear current contact
+
+	// Update contact
+
+	// Filter contacts
+
+	// Clear filter
+
+	return (
+		<ContactContext.provider
+			value={{
+				contacts : state.contacts
+			}}
+		>
+			{props.children}
+		</ContactContext.provider>
+	);
+};
