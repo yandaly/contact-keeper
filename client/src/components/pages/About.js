2d0fd12bc6aef499ad106366c05ad04a4c363d3d
yandaly <thegreygringo@gmail.com>
2019-12-02 14:37:46 +0100

    Cleanup, navbar and router

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => {
+	return (
+		<div>
+			<h1>About This App</h1>
+			<p className='my-1'>This is a full stack app for keeping contact.</p>
+			<p className='bg-dark p'>
+				<strong>Version:</strong> 0.0.1
+			</p>
+		</div>
+	);
+};
