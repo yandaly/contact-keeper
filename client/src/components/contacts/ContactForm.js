662916b36888cb3b2ca8396465ccf1029ab95250
yandaly <thegreygringo@gmail.com>
2019-12-02 18:22:06 +0100

    fin de journée

---
+++ client/src/components/contacts/ContactForm.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const ContactForm = () => {
+	const [
+		contact,
+		setContact
+	] = useState({
+		name  : '',
+		email : '',
+		phone : '',
+		type  : 'personal'
+	});
+
+	return <div />;
+};
+
+export default ContactForm;
